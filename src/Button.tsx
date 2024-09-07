18865951341962144a00dc85a5274c5416a220b5
Eliam2102 <157552853+Eliam2102@users.noreply.github.com>
2024-09-06 20:15:37 -0600

    Commit final
    En este commit se suben los cambios con las funcionalidades finales

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,11 @@
-function Button({value, onclick}: {value: string; onclick:() => void}){
-    return <button onClick={onclick}>{value}</button>
-}
+import React from "react";
+import {ButtonProps} from  './ButtonProps'; //importamos la interface desde su propio archivo
+
+
+const Button: React.FC<ButtonProps>=({value, onClick}) =>{
+    return ( 
+        <button className="operator" onClick={onClick}>{value}</button>
+    );
+};
 
 export default Button;
--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import Button from './Button';
+import './App.css'//importar el archivo css
 
 function Calculator() {
-  const [displayValue, setDisplayValue] = useState("0");
+  const [displayValue, setDisplayValue] = useState<string>("0");
   const [operator, setOperator] = useState<string | null>(null);
   const [firstOperand, setFirstOperand] = useState<number | null>(null);
 
@@ -47,11 +48,42 @@ function Calculator() {
     }
   }
 
+  function handleClearClick(){
+    setDisplayValue('0');
+    setFirstOperand(null);
+    setOperator(null);
+}
+
   return (
     <div>
-      <div> {displayValue} </div>
-      <Button value="1" onclick={() => handleNumberClick('1')} />{' '}
-      <Button value="+" onclick={() => handleOperationClick('+')} />{' '}
+      <div className="calculator">
+        {''}
+        <div className='display'> {displayValue}</div>{''}
+        <div>
+          <Button value = "7" onClick={() => handleNumberClick('7')}/>
+          <Button value = "4" onClick={() => handleNumberClick('4')}/>
+          <Button value = "1" onClick={() => handleNumberClick('1')}/>
+          <Button value = "0" onClick={() => handleNumberClick('0')}/>
+        </div>
+        <div>
+        <Button value = "8" onClick={() => handleNumberClick('8')}/>
+        <Button value = "5" onClick={() => handleNumberClick('5')}/>
+        <Button value = "2" onClick={() => handleNumberClick('2')}/>
+        <Button value = "=" onClick={calculate}/>
+        </div>
+        <div>
+        <Button value = "9" onClick={() => handleNumberClick('9')}/>
+        <Button value = "6" onClick={() => handleNumberClick('6')}/>
+        <Button value = "3" onClick={() => handleNumberClick('3')}/>
+        <Button value = "-" onClick={() => handleNumberClick('-')}/>
+        </div>
+        <div>
+        <Button value = "C" onClick={handleClearClick}/>
+        <Button value = "/" onClick={() => handleOperationClick('/')}/>
+        <Button value = "*" onClick={() => handleOperationClick('*')}/>
+        <Button value = "+" onClick={() => handleOperationClick('+')}/>
+        </div>
+      </div>
     </div>
   );
 }
